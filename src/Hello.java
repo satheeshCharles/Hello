89ed2b83368e64c858cd01e80d829ea49e4f56da
satheeshcharles <satheesh.charles@gmail.com>
2020-07-26 11:03:09 +0530

    operator demo

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,13 @@
 public class Hello {
-    public static void main (String args[]) {
+    public static void main(String [] args) {
         System.out.println("Hello World of Remastering");
-        int initialValue=7;
-        System.out.println(initialValue);
+        int initialValue = (7 + 8) * (6/2);
+        int modifiedValue = (6*2);
+        int updatedValue = initialValue * 2;
+        int lastValue = initialValue+modifiedValue+updatedValue;
+        int finalValue = 1000-lastValue;
+        System.out.println("The last Value is "+ lastValue);
+        System.out.println("The last Value dynamic "+ (1000- lastValue));
+        System.out.println(finalValue);
     }
 }
