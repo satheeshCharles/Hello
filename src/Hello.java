3ca5f20364656d43f7b8e8a4b98250a64d7be8b2
satheeshcharles <satheesh.charles@gmail.com>
2020-07-26 23:55:50 +0530

    calculating min value

--- src/Hello.java
+++ src/Hello.java
@@ -9,5 +9,25 @@ public class Hello {
         System.out.println("The last Value is " + lastValue);
         System.out.println("The last Value dynamic " + (1000 - lastValue));
         System.out.println(finalValue);
+
+        float findMinValue= Float.MIN_VALUE;
+        float findMaxvalue = Float.MAX_VALUE;
+        System.out.println("Min Value " + findMinValue + " Max value "+ findMaxvalue);
+        double checkFloatNum = 1.78 ;
+        System.out.println(checkFloatNum);
+
+        int findMiniValue= Integer.MIN_VALUE;
+        int findMaxivalue = Integer.MAX_VALUE;
+        System.out.println("Min Value " + findMiniValue + " Max value "+ findMaxivalue);
+        System.out.println("Min i value +1 = "+ (findMaxivalue-1));
+        System.out.println("Min i value +1 = "+ (findMaxivalue+1));
+        int checkintegerNum = 1 ;
+        System.out.println(checkintegerNum);
+
+        int demo = 0;
+        System.out.println(demo);
+
     }
+
+
 }
