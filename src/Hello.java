1e0699fb5f2771536a01e408b8b9a82b3a87b7c0
satheeshcharles <satheesh.charles@gmail.com>
2020-07-26 10:36:03 +0530

    print int

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main (String args[]) {
         System.out.println("Hello World of Remastering");
-
+        int initialValue=7;
+        System.out.println(initialValue);
     }
 }
